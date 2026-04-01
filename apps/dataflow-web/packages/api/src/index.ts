4c94cab70faf35c5b74acf0d177014b280e9a8e1
顾海霞 <haixiagu68@gmail.com>
2026-04-01 09:49:02 +0800

    feat(doc-audit-client): add module doc-audit-client (#50)
    * feat(doc-audit-client): add module doc-audit-client
    
    * add version & charts
    
    * 补充提交
    
    * 屏蔽 已退回 筛选项
    
    * 接口替换
    
    * 修复bug
    
    * 记住pageSize
    
    * 修复加签、转审无法选择用户的问题
    
    * 内容为空时，不显示详情侧边栏
    
    ---------
    
    Co-authored-by: 顾海霞（Hailey） <gu.haixia@aishu.cn>

--- apps/dataflow-web/packages/api/src/index.ts
+++ apps/dataflow-web/packages/api/src/index.ts
@@ -11,185 +11,187 @@ import { DefaultApi as PersonalConfigApi } from "./personal-config";
 import { DefaultApi as MetadataApi } from "./metadata";
 
 export interface ApiError {
-    cause: string;
-    code: number;
-    message: string;
+  cause: string;
+  code: number;
+  message: string;
 }
 
 export interface ApiConfig {
-    businessDomainID: string;
-    baseURL: string;
-    token: string;
-    language: string;
-    pathPrefix?: string;
-    isRefreshable(err: AxiosError<ApiError>): boolean;
-    refreshToken?(): Promise<string> | string;
+  businessDomainID: string;
+  baseURL: string;
+  token: string;
+  getToken: () => string;
+  language: string;
+  pathPrefix?: string;
+  isRefreshable(err: AxiosError<ApiError>): boolean;
+  refreshToken?(): Promise<string> | string;
 }
 
 export class Api {
-    private readonly _axios: AxiosInstance;
+  private readonly _axios: AxiosInstance;
 
-    private readonly _oauth2: Oauth2Api;
+  private readonly _oauth2: Oauth2Api;
 
-    private readonly _efast: EfastApi;
+  private readonly _efast: EfastApi;
 
-    private readonly _docCollection: DocCollectionApi;
+  private readonly _docCollection: DocCollectionApi;
 
-    private readonly _automation: AutomationApi;
+  private readonly _automation: AutomationApi;
 
-    private readonly _workflow: WorkflowApi;
+  private readonly _workflow: WorkflowApi;
 
-    private readonly _openDoc: OpenDocApi;
+  private readonly _openDoc: OpenDocApi;
 
-    private readonly _appStore: AppStoreApi;
+  private readonly _appStore: AppStoreApi;
 
-    private readonly _personalConfig: PersonalConfigApi;
-    private readonly _metadata: MetadataApi;
+  private readonly _personalConfig: PersonalConfigApi;
+  private readonly _metadata: MetadataApi;
 
-    private _config: ApiConfig = {
-        baseURL: window.location.origin,
-        token: "",
-        language: "zh-cn",
-        isRefreshable: () => false,
-        businessDomainID: "",
-    };
+  private _config: ApiConfig = {
+    baseURL: window.location.origin,
+    token: "",
+    getToken: () => "",
+    language: "zh-cn",
+    isRefreshable: () => false,
+    businessDomainID: "",
+  };
 
-    private refreshPromise: any = null;
+  private refreshPromise: any = null;
 
-    constructor(config: Partial<ApiConfig> = {}) {
-        this.setup(config);
-        this._axios = axios.create();
-        this._axios.interceptors.request.use(async (conf) => {
-            if (!conf.baseURL && conf.url && /^(\/)?api/.test(conf.url)) {
-                conf.baseURL = this.getBasePath();
-            }
+  constructor(config: Partial<ApiConfig> = {}) {
+    this.setup(config);
+    this._axios = axios.create();
+    this._axios.interceptors.request.use(async (conf) => {
+      if (!conf.baseURL && conf.url && /^(\/)?api/.test(conf.url)) {
+        conf.baseURL = this.getBasePath();
+      }
 
-            if (!conf.headers) {
-                conf.headers = {};
-            }
+      if (!conf.headers) {
+        conf.headers = {};
+      }
 
-            if (conf.method === "get") {
-                const { allowTimestamp = true } = conf;
-                if (allowTimestamp) {
-                    const separator = conf.url.indexOf("?") === -1 ? "?" : "&";
-                    conf.url += separator + "_t=" + new Date().getTime();
-                }
-            }
-
-            conf.headers.authorization = `Bearer ${this._config.token}`;
-            conf.headers["X-Language"] = this._config.language;
-            conf.headers["x-business-domain"] = this._config.businessDomainID;
-
-            return conf;
-        });
-        this._axios.interceptors.response.use(
-            (res) => res,
-            async (err: AxiosError<ApiError>) => {
-                if (
-                    this._config.isRefreshable(err) &&
-                    typeof this._config.refreshToken === "function"
-                ) {
-                    try {
-                        if (!this.refreshPromise) {
-                            this.refreshPromise = this._config.refreshToken();
-                        }
-                        this._config.token = await this.refreshPromise;
-                        return this._axios.request(err.config);
-                    } catch (_) {
-                        throw err;
-                    } finally {
-                        this.refreshPromise = null;
-                    }
-                }
-                throw err;
-            }
-        );
-        this._oauth2 = this.createApi(Oauth2Api);
-        this._efast = this.createApi(EfastApi, "/api");
-        this._docCollection = this.createApi(
-            DocCollectionApi,
-            "/api/file-collector/v1"
-        );
-        this._automation = this.createApi(AutomationApi, "/api/automation/v1");
-        this._workflow = this.createApi(WorkflowApi, "/api");
-        this._openDoc = this.createApi(OpenDocApi);
-        this._appStore = this.createApi(AppStoreApi);
-        this._personalConfig = this.createApi(
-            PersonalConfigApi,
-            "/api/personal-config/v1"
-        );
-        this._metadata = this.createApi(MetadataApi, "/api");
-    }
-
-    public setup(config: Partial<ApiConfig>) {
-        Object.assign(this._config, config);
-    }
-
-    private getBasePath() {
-        return this._config.baseURL + this._config.pathPrefix;
-    }
-
-    get oauth2() {
-        return this._oauth2;
-    }
-
-    get efast() {
-        return this._efast;
-    }
-
-    get docCollection() {
-        return this._docCollection;
-    }
-
-    get automation() {
-        return this._automation;
-    }
-
-    get workflow() {
-        return this._workflow;
-    }
-
-    get openDoc() {
-        return this._openDoc;
-    }
-
-    get appStore() {
-        return this._appStore;
-    }
-
-    get personalConfig() {
-        return this._personalConfig;
-    }
-    get metadata() {
-        return this._metadata;
-    }
-
-    /**
-     * 直接使用axios加url调用接口时需考虑富客户端请求场景
-     * 可通过prefixUrl拼接url获得真实路径
-     * 如果 url 为 api 或 /api 且未设置 baseUrl, 会将
-     * baseUrl 设为 this.getBasePath()
-     */
-    get axios() {
-        return this._axios;
-    }
-
-    private createApi<
-        T,
-        C extends {
-            new(
-                configuration?: Configuration,
-                basePath?: string,
-                axios?: AxiosInstance
-            ): T;
+      if (conf.method === "get") {
+        const { allowTimestamp = true } = conf;
+        if (allowTimestamp) {
+          const separator = conf.url.indexOf("?") === -1 ? "?" : "&";
+          conf.url += separator + "_t=" + new Date().getTime();
         }
-    >(Ctor: C, prefix: string = "") {
-        const instance = new Ctor(undefined, undefined, this._axios);
-        Object.defineProperty(instance, "basePath", {
-            get: () => this.getBasePath() + prefix,
-        });
-        return instance;
-    }
+      }
+
+      conf.headers.authorization = `Bearer ${this._config.getToken?.() || this._config.token}`;
+      conf.headers["X-Language"] = this._config.language;
+      conf.headers["x-business-domain"] = this._config.businessDomainID;
+
+      return conf;
+    });
+    this._axios.interceptors.response.use(
+      (res) => res,
+      async (err: AxiosError<ApiError>) => {
+        if (
+          this._config.isRefreshable(err) &&
+          typeof this._config.refreshToken === "function"
+        ) {
+          try {
+            if (!this.refreshPromise) {
+              this.refreshPromise = this._config.refreshToken();
+            }
+            this._config.token = await this.refreshPromise;
+            return this._axios.request(err.config);
+          } catch (_) {
+            throw err;
+          } finally {
+            this.refreshPromise = null;
+          }
+        }
+        throw err;
+      },
+    );
+    this._oauth2 = this.createApi(Oauth2Api);
+    this._efast = this.createApi(EfastApi, "/api");
+    this._docCollection = this.createApi(
+      DocCollectionApi,
+      "/api/file-collector/v1",
+    );
+    this._automation = this.createApi(AutomationApi, "/api/automation/v1");
+    this._workflow = this.createApi(WorkflowApi, "/api");
+    this._openDoc = this.createApi(OpenDocApi);
+    this._appStore = this.createApi(AppStoreApi);
+    this._personalConfig = this.createApi(
+      PersonalConfigApi,
+      "/api/personal-config/v1",
+    );
+    this._metadata = this.createApi(MetadataApi, "/api");
+  }
+
+  public setup(config: Partial<ApiConfig>) {
+    Object.assign(this._config, config);
+  }
+
+  private getBasePath() {
+    return this._config.baseURL + this._config.pathPrefix;
+  }
+
+  get oauth2() {
+    return this._oauth2;
+  }
+
+  get efast() {
+    return this._efast;
+  }
+
+  get docCollection() {
+    return this._docCollection;
+  }
+
+  get automation() {
+    return this._automation;
+  }
+
+  get workflow() {
+    return this._workflow;
+  }
+
+  get openDoc() {
+    return this._openDoc;
+  }
+
+  get appStore() {
+    return this._appStore;
+  }
+
+  get personalConfig() {
+    return this._personalConfig;
+  }
+  get metadata() {
+    return this._metadata;
+  }
+
+  /**
+   * 直接使用axios加url调用接口时需考虑富客户端请求场景
+   * 可通过prefixUrl拼接url获得真实路径
+   * 如果 url 为 api 或 /api 且未设置 baseUrl, 会将
+   * baseUrl 设为 this.getBasePath()
+   */
+  get axios() {
+    return this._axios;
+  }
+
+  private createApi<
+    T,
+    C extends {
+      new (
+        configuration?: Configuration,
+        basePath?: string,
+        axios?: AxiosInstance,
+      ): T;
+    },
+  >(Ctor: C, prefix: string = "") {
+    const instance = new Ctor(undefined, undefined, this._axios);
+    Object.defineProperty(instance, "basePath", {
+      get: () => this.getBasePath() + prefix,
+    });
+    return instance;
+  }
 }
 
 export default new Api();
